2420561fcb55a4bbd200991e28f8f69c02698ae1
Nikita <nikita-reutov@mail.ru>
2020-03-04 00:30:39 +0300

    fix invalid date

--- frontend/src/components/trainers/trainers_visitations.js
+++ frontend/src/components/trainers/trainers_visitations.js
@@ -14,7 +14,8 @@ const TrainersVisitations = ({trainers, visitations}) => {
             {visitations[trainer].map((i, key) => {
               return (
                 <div className={"has-margin-left-7"} key={key}>
-                  <b>{i.type_display[0]}</b> {i.customer.id} {i.customer.full_name} ({moment(i.came_at).format('dddd, D MMMM в H:mm')})
+                  <b>{i.type_display[0]}</b> {i.customer.id} {i.customer.full_name + ' '}
+                  ({moment(i.came_at, 'DD/MM/YYYY HH:mm:ss').format('dddd, D MMMM в H:mm')})
                   </div>
               );
             })}
