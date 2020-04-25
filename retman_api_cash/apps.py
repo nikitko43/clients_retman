06f27de33c0066b66b5d4614825e8a4b4cc66833
Nikita <nikita-reutov@mail.ru>
2020-04-25 16:10:37 +0300

    delete old frontend and api

--- retman_api_cash/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RetmanApiCashConfig(AppConfig):
-    name = 'retman_api_cash'
--- retman_crm/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RetmanCrmConfig(AppConfig):
-    name = 'retman_crm'
